7da67dad1ec9779820d057164694577a67b4221f
Justas Masiulis <Justas.masiulis@yahoo.com>
2020-03-20 18:46:38 +0200

    updated license date

--- include/lazy_importer.hpp
+++ include/lazy_importer.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Justas Masiulis
+ * Copyright 2018-2020 Justas Masiulis
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
